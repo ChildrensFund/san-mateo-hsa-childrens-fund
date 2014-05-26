52dc7b8b93c79ad512282b4eace27834dfd0c546
Amar Patel <theamarpatel@gmail.com>
2014-05-26 14:06:23 -0700

    non-authorized users can't access protected resources

--- client/app.js
+++ client/app.js
@@ -86,7 +86,7 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 
 }])
 
-.controller('inputController', ['$scope', 'restful', '$location', function ($scope, restful, $location) {
+.controller('inputController', ['$scope', 'restful', 'protect', function ($scope, restful, protect) {
 
   $scope.tempChildItemObj = {
     item: undefined, 
@@ -434,4 +434,63 @@ temporary object
   }
 
 }])
+
+// verifies worker views
+.directive('workerVerify', ['protect', '$location', function (protect, $location) {
+  return {
+    restrict: 'A',
+    link: function () {
+      protect('workers').then(function (isWorker) {
+        if (isWorker === false) {
+          console.log('workerVerify changed path to /workers/signin');
+          $location.path('/workers/signin');
+        } else {
+          console.log('workerVerify allowed access');
+        }
+      })
+    }
+  }
+}])
+
+// verifies donor views
+.directive('donorVerify', ['protect', '$location', function (protect, $location) {
+  return {
+    restrict: 'A',
+    link: function () {
+      protect('donors').then(function (isWorker) {
+        if (isWorker === false) {
+          console.log('donorVerify changed path to /donors/signin');
+          $location.path('/donors/signin');
+        } else {
+          console.log('donorVerify allowed access');
+        }
+      })
+    }
+  }
+}])
+
+// verifies admin views
+.directive('adminVerify', ['protect', '$location', function (protect, $location) {
+  return {
+    restrict: 'A',
+    link: function () {
+      protect('admin').then(function (isWorker) {
+        if (isWorker === false) {
+          console.log('donorVerify changed path to /admin/signin');
+          $location.path('/admin/signin');
+        } else {
+          console.log('donorVerify allowed access');
+        }
+      })
+    }
+  }
+}])
+
+
+
+
+
+
+
+
 
