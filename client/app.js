4fb271ffeb4daca3211221cfa999c6ba6c2f03e6
Amar Patel <theamarpatel@gmail.com>
2014-05-25 19:12:59 -0700

    updated donorView, childInputView, and childrenFeedView; updated pledge and post function in inputController

--- client/app.js
+++ client/app.js
@@ -41,36 +41,34 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
     }
   })
 
-
-  // view for workers to update an existing new child tag
 }])
 
-.controller('inputController', ['$scope', 'restful', function ($scope, restful) {
+.controller('inputController', ['$scope', 'restful', '$location', function ($scope, restful, $location) {
 
-  $scope.tempChildObj = {
-    items: [ {item: undefined, price: undefined}, {item: undefined, price: undefined}, {item: undefined, price: undefined} ]
+  $scope.tempChildItemObj = {
+    item: undefined, 
+    status: undefined, 
+    price: undefined, 
+    paymentDate: undefined, 
+    hsaReceivedDate: undefined, 
+    childReceivedDate: undefined
   };
 
-  // $scope.post = function () {
-  //   if ($scope.childName && $scope.item1) {
-  //     restful.createChild($scope.childName, $scope.item1).then( function (promise) {
-  //       if (promise) {
-  //         // Clears input fields after GET succeeds
-  //         $scope.childName = '';
-  //         $scope.item1 = '';
-  //         // calls GET after POST is complete
-  //         return $scope.get();
-  //       }
-  //     });
-  //   } else {
-  //     console.log('Invalid Input Values')
-  //   }
-  // };
+  // sets basic template for creating new child tags
+  $scope.tempChildObj = {
+    request: {
+      // had to use extend otherwise all item field populate simultaneously 
+      items: [_.extend({},$scope.tempChildItemObj), _.extend({},$scope.tempChildItemObj), _.extend({},$scope.tempChildItemObj)] 
+    }
+  };
 
-  $scope.post = function (childObj, reqObj, workerObj) {
-    // restful.createChild(childObj, reqObj, workerObj);
-    restful.createChild($scope.tempChildObj);
-    // console.log($scope.tempChildObj);
+  $scope.post = function (childObj) {
+    $scope.tempChildObj.request.createdAt = new Date();
+    restful.createChild($scope.tempChildObj).then(function (promise) {
+      if (promise) {
+        return $scope.get();
+      }
+    });
   };
 
   $scope.get = function () {
@@ -84,7 +82,7 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 
   $scope.pledge = function (childObj, index) {
     // for a given child's item, sets pledged to true
-    childObj.childData.items[index].pledged = true;
+    childObj.request.items[index].status = 'pledged';
 
     // then POSTs it to the server
     restful.updateChild(childObj).then(function (promise) {
@@ -111,7 +109,7 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
         data: childObj
       }).success(function (data, status) {
         console.log('(Create) POST Success! ', data);
-        // return data;
+        return data;
       }).error(function (data, status) {
         console.log('(Create) POST Error! ', data, status);
       });
@@ -145,76 +143,77 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 }])
 
 
+/*
 
-// temporary object
-// {
-//   child: {
-//     firstName: 'Amar',
-//     lastName: 'Patel',
-//     phone: '19166008929',
-//     gender: 'male',
-//     dob: 'my birthday',
-//     age: 24,
-//     location: 'San Francisco',
-//     CFID: '00001A',
-//     programArea: 'somewhere',
-//     bio: 'Amar hails from a heritage of awesome and bad-assery.'
-//   },
+temporary object
+var masterObj =
+{
+  child: {
+    firstName: 'Amar',
+    lastName: 'Patel',
+    phone: '19166008929',
+    gender: 'male',
+    dob: 'my birthday',
+    age: 24,
+    location: 'San Francisco',
+    CFID: '00001A',
+    programArea: 'somewhere',
+    bio: 'Amar hails from a heritage of awesome and bad-assery.'
+  },
 
-//   request: {
-//     createdAt: 'dateString',
-//     items: [
-//       {
-//         item: 'bike', 
-//         status: null, 
-//         price: null, 
-//         date: 'itemReceivedDate', 
-//         paymentDate: 'somedate',
-//         hsaReceivedDate: 'somedate',
-//         childReceivedDate: 'somedate'
-//       },
-//       {
-//         item: 'helmet', 
-//         status: null, 
-//         price: null, 
-//         date: 'itemReceivedDate', 
-//         paymentDate: 'somedate',
-//         hsaReceivedDate: 'somedate',
-//         childReceivedDate: 'somedate'
-//       },
-//       {
-//         item: 'babes', 
-//         status: null, 
-//         price: null, 
-//         date: 'itemReceivedDate', 
-//         paymentDate: 'somedate',
-//         hsaReceivedDate: 'somedate',
-//         childReceivedDate: 'somedate'
-//       }
-//     ]
-//   },
+  request: {
+    createdAt: 'dateString',
+    items: [
+      {
+        item: 'bike', 
+        status: null, 
+        price: null, 
+        paymentDate: 'somedate',
+        hsaReceivedDate: 'somedate',
+        childReceivedDate: 'somedate'
+      },
+      {
+        item: 'helmet', 
+        status: null, 
+        price: null, 
+        paymentDate: 'somedate',
+        hsaReceivedDate: 'somedate',
+        childReceivedDate: 'somedate'
+      },
+      {
+        item: 'babes', 
+        status: null, 
+        price: null, 
+        paymentDate: 'somedate',
+        hsaReceivedDate: 'somedate',
+        childReceivedDate: 'somedate'
+      }
+    ]
+  },
 
-//   worker: {
-//     firstName: 'Wayland',
-//     lastName 'Gangsta-fresh',
-//     phone: '12903812124',
-//     email: 'someemail',
-//     department: 'someDept',
-//     supervisorFirstName: 'Marcus',
-//     supervisorLastName: 'Phillips',
-//     coordinatorFirstName: 'Tony',
-//     coordinatorLastName 'Phillips'
-//   },
+  worker: {
+    firstName: 'Wayland',
+    lastName: 'Gangsta-fresh',
+    phone: '12903812124',
+    email: 'someemail',
+    department: 'someDept',
+    supervisorFirstName: 'Marcus',
+    supervisorLastName: 'Phillips',
+    coordinatorFirstName: 'Tony',
+    coordinatorLastName 'Phillips'
+  },
 
-//   donor: {
-//     email: 'someemail',
-//     address1: '1221 1st ave.',
-//     address1: 'apt se1801',
-//     city: 'seattle',
-//     state: 'washington',
-//     zip: '98101',
-//     firstName: 'amar',
-//     lastName: 'patel',
-//     phone: '9166008929'
-//   }
-// }
+  donor: {
+    firstName: 'amar',
+    lastName: 'patel',
+    phone: '9166008929',
+    email: 'someemail',
+    address1: '1221 1st ave.',
+    address2: 'apt se1801',
+    city: 'seattle',
+    state: 'washington',
+    zip: '98101'
+  }
+};
+
+*/
