156c4b3d1f3e05c38830001247bbdbf59d33f47a
Evan Kim <evan.kim.390@gmail.com>
2014-05-25 23:03:57 -0700

    Create signedIn module at server/util/auth.js

---
+++ server/util/auth.js
@@ -0,0 +1,66 @@
+var Donor = require('../config/mysql_config.js').Donor;
+var Staff = require('../config/mysql_config.js').Staff;
+var Admin = require('../config/mysql_config.js').Admin;
+var q = require('q');
+
+var setUserType = function(request, response, userType){
+  var userType = userType || request.body.userType;
+  switch(userType){
+    case 'donors':
+      return Donor;
+      break;
+    case 'workers':
+      return Staff;
+      break;
+    case 'admin':
+      return Admin;
+      break;
+    default:
+      return undefined;
+      break;
+  }
+}
+
+module.exports = function(request){
+  console.log('################## Server-Side: signedIn Function Called ##################');
+  var deferred = q.defer();
+  var sessionToken = request.cookies.sessionToken;
+  var userType = request.cookies.type;
+  var whitelist = Array.prototype.slice.call(arguments, 1);
+
+  if(!sessionToken || !userType || sessionToken === 'j:null' || userType === 'j:null'){
+    console.log('Signed out (no session token or no userType)');
+    console.log('Session Token:', sessionToken);
+    console.log('User Type:', userType);
+    deferred.resolve(false);
+  } else {
+
+    var User = setUserType(request, null, userType);
+    User.find({where: {sessionToken: sessionToken}}).success(function(user){
+      if(!user){
+        console.log('Complete: User not signed in');
+        deferred.resolve(false);
+      } else {
+        console.log('Complete: User is signed in');
+        console.log('Checking to see if whitelisted');
+        var found = false;
+        for(var i = 0; i < whitelist.length; i++){
+          if(whitelist[i] === userType){
+            console.log('User is authorized');
+            deferred.resolve(true);
+            found = true;
+          }
+        }
+        if(found === false){
+          console.log('User is not whitelisted, unauthorized');
+          deferred.resolve(false);
+        }
+      }
+    }).error(function(err){
+      console.log('Complete: Server error in db lookup');
+      deferred.reject();
+    });
+  }
+
+  return deferred.promise;
+};
