5f5d1b4a3a9791331f2766feb7353ec88e808bcd
Evan Kim <evan.kim.390@gmail.com>
2014-05-27 19:58:13 -0700

    Break app.js into controllers/directives/services

---
+++ client/js/controllers.js
@@ -0,0 +1,155 @@
+app.controller('appCtrl', function($state, $scope, $rootScope){
+  $rootScope.$on('$stateChangeStart', function(){
+    console.log('state change detected');
+  })
+})
+
+
+.controller('inputController', ['$scope', 'restful', 'protect', function ($scope, restful, protect) {
+
+  $scope.tempChildItemObj = {
+    item: undefined, 
+    status: undefined, 
+    price: undefined, 
+    paymentDate: undefined, 
+    hsaReceivedDate: undefined, 
+    childReceivedDate: undefined
+  };
+
+  // sets basic template for creating new child tags
+  $scope.tempChildObj = {
+    request: {
+      // had to use extend otherwise all item field populate simultaneously 
+      items: [_.extend({},$scope.tempChildItemObj), _.extend({},$scope.tempChildItemObj), _.extend({},$scope.tempChildItemObj)] 
+    }
+  };
+
+  $scope.post = function () {
+    // $scope.tempChildObj.worker.accountID = get signed-in worker's account ID here
+    $scope.tempChildObj.request.createdAt = new Date();
+    restful.createChild($scope.tempChildObj).then(function (promise) {
+      if (promise) {
+        return $scope.get();
+      }
+    });
+  };
+
+  $scope.get = function () {
+    restful.getChildren().then(function (promise) {
+      if (promise) {
+        // sets $scope.children as received server data
+        $scope.children = promise.data;
+      }
+    });
+  };
+
+  $scope.pledge = function (childObj, index) {
+    // for a given child's item, sets pledged to true
+    childObj.request.items[index].status = 'pledged';
+
+    // then POSTs it to the server
+    restful.updateChild(childObj).then(function (promise) {
+      if (promise) {
+
+        //after POST is completed, GETs updated data
+          //necessary to updated Pledge/Pledged! buttons
+        $scope.get();
+      }
+    });
+  }
+
+}])
+
+//Authentication logic
+.controller('authController', ['$scope', '$http', '$state', function($scope, $http, $state){
+  $scope.signup = function(){
+    if($scope.password === $scope.passwordConfirmation){
+      $http({
+        method: 'POST',
+        url: '/auth/signup',
+        data: {
+          userType: $state.current.data.userType,
+          email: $scope.email,
+          password: $scope.password
+        }
+      }).success(function(data, status){
+        console.log('User Created!');
+      }).error(function(data, status){
+        console.log('User Not Created: Server Error');
+      })
+    }
+  };
+
+  $scope.signin = function(){
+    if($scope.password){
+      $http({
+        method: 'POST',
+        url: '/auth/signin',
+        data: {
+          userType: $stateParams.userType,
+          email: $scope.email,
+          password: $scope.password
+        }
+      }).success(function(data, status){
+        console.log('User signed in');
+      }).error(function(data, status){
+        console.log('User not signed in: Server Error');
+      });
+    }
+  }
+
+  $scope.signout = function(){
+    $http({
+      method: 'POST',
+      url: '/auth/signout'
+    }).success(function(data, status){
+      console.log('User signed out');
+    }).error(function(data, status){
+      console.log('User not signed out: Server Error');
+    });
+  }
+
+  $scope.signedIn = function(){
+    $http({
+      method: 'POST',
+      url: '/auth/signedIn',
+      data: {
+        userType: $state.current.data.userType,
+      }
+    }).success(function(data, status){
+    }).error(function(data, status){
+    });
+  }
+
+  $scope.sendReset = function(){
+    $http({
+      method: 'POST',
+      url: '/auth/sendReset',
+      data: {
+        userType: $state.current.data.userType,
+        email: $scope.email
+      }
+    }).success(function(data, status){
+      console.log('Reset token sent');
+    }).error(function(data, status){
+      console.log('Reset token not sent: Server Error');
+    });
+  }
+
+  $scope.resetPassword = function(){
+    $http({
+      method: 'POST',
+      url: 'auth/resetPassword',
+      data: {
+        userType: $state.current.data.userType,
+        password: $scope.password,
+        resetToken: $stateParams.resetToken
+      }
+    }).success(function(data, status){
+      console.log('Password successfully reset');
+    }).error(function(data, status){
+      console.log('Password not reset: Server Error');
+    });
+  }
+
+}])
---
+++ client/js/services.js
@@ -0,0 +1,105 @@
+// GET/POST logic
+app.factory('restful', ['$http', function ($http) {
+  return {
+    createChild: function (childObj) {
+      return $http({
+        method: 'POST',
+        url: '/children',
+        data: childObj
+      }).success(function (data, status) {
+        console.log('(Create) POST Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(Create) POST Error! ', data, status);
+      });
+    },
+
+    updateChild: function (childObj) {
+      return $http({
+        method: 'POST',
+        url: '/children',
+        data: childObj
+      }).success(function (data, status) {        
+        console.log('(Update) POST Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(Update) POST Error! ', data, status);
+      });
+    },
+
+    getChildren: function () {
+      return $http({
+        method: 'GET',
+        url: '/children'
+      }).success(function (data, status) {
+        console.log('GET Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('GET Error! ', data, status);
+      });
+    }
+  }
+}])
+
+.factory('sessionCache', function(){
+  var cache = {};
+  var sessionCacheService = {};
+  sessionCacheService.updateSessionToken = function(sessionToken){
+    cache.sessionToken = sessionToken;
+  };
+
+  sessionCacheService.updateUserType = function(userType){
+    cache.userType = userType;
+  };
+
+  sessionCacheService.retrieveSessionToken = function(){
+    return cache.sessionToken;
+  };
+
+  sessionCacheService.retrieveUserType = function(){
+    return cache.userType;
+  };
+  return sessionCacheService;
+})
+
+.factory('protect', ['$cookies', '$q', '$http', 'sessionCache', function($cookies, $q, $http, sessionCache){
+  var cookieSessionToken = $cookies.sessionToken;
+  var cookieUserType = $cookies.type;
+ 
+  //pageType needs to be 'donors' || 'workers' || 'admin'
+  return function(pageType){
+    console.log('################ Page is protected, checking login status ################');
+    var deferred = $q.defer();
+    if(!cookieSessionToken || !cookieUserType || cookieSessionToken === 'j:null' || cookieUserType === 'j:null'){
+      console.log('Session token is null');
+      deferred.resolve(false);
+    } else if (sessionCache.retrieveSessionToken() === cookieSessionToken && sessionCache.retrieveUserType() === cookieUserType && cookieUserType === pageType) {
+      console.log('Cached credentials');
+      deferred.resolve(true);
+    } else {
+      console.log('Client has sessionId cookie, but no sessionId is cached in Angular. Awaiting server verification');
+      $http({
+        method: 'POST',
+        url: '/auth/signedIn',
+        data: {
+          userType: pageType,
+        }
+      }).success(function(data, status){
+        if(data.signedIn){
+          console.log('Client authorized');
+          sessionCache.updateSessionToken(cookieSessionToken);
+          sessionCache.updateUserType(cookieUserType);
+          deferred.resolve(true);
+        } else {
+          console.log('Client unauthorized');
+          deferred.resolve(false);
+        }
+      }).error(function(){
+        console.log('Server error: Preventing client access regardless');
+        deferred.reject('There was a server error');
+      });
+    }
+    return deferred.promise;
+  }
+
+}])
