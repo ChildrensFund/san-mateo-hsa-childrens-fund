3428d5de1c7224c7527abf3896653a50b2b83c44
Amar Patel <theamarpatel@gmail.com>
2014-05-30 13:07:27 -0700

    updated master obj based on new project scope

--- client/js/app.js
+++ client/js/app.js
@@ -222,93 +222,13 @@ app.config(function($stateProvider, $locationProvider, $urlRouterProvider){
 
 });
 
-// ui-router configuration
-// app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-//   $urlRouterProvider.otherwise('/');
-
-//   $stateProvider
-//   // root view is for donors
-//   .state('/', {
-//     url: '/',
-//     views: {
-//       navMenuView: { templateUrl: '/templates/developerViews/navMenu.html'},
-//       firstView: { templateUrl: 'templates/userViews/donorView.html', controller: 'inputController' },
-//       secondView: { templateUrl: 'templates/userViews/donorChildrenFeedView.html', controller: 'inputController' }
-//     }
-//   })
-
-//   // view for workers (nav bar, input field and list of children)
-//   .state('workersPortal', {
-//     url: '/workers',
-//     views: {
-//       navMenuView: { templateUrl: '/templates/developerViews/navMenu.html'},
-//       firstView: { templateUrl: 'templates/userViews/workerView.html', controller: 'inputController' },
-//       secondView: { templateUrl: 'templates/userViews/workerChildrenFeedView.html', controller: 'inputController' }
-//     }
-//   })
-
-//   // view for workers to create a new child tag
-//   .state('createTag', {
-//     url: '/create',
-//     views: {
-//       navMenuView: { templateUrl: '/templates/developerViews/navMenu.html'},
-//       firstView: { templateUrl: 'templates/userViews/workerChildInputView.html', controller: 'inputController' }
-//     }
-//   })
-
-//   //Authentication Handlers
-//   .state('signup', {
-//     url: '/{userType:donors|workers|admin}/signup',
-//     views: {
-//       navMenuView: { templateUrl: '/templates/developerViews/navMenu.html' },
-//       firstView: { templateUrl: 'templates/authentication/signupView.html', controller: 'authController' }
-//     }
-//   })
-
-//   .state('signin', {
-//     url: '/{userType:donors|workers|admin}/signin',
-//     views: {
-//       navMenuView: { templateUrl: '/templates/developerViews/navMenu.html' },
-//       firstView: { templateUrl: 'templates/authentication/signinView.html', controller: 'authController' }
-//     }
-//   })
-
-//   .state('signout', {
-//     url: '/{userType:donors|workers|admin}/signout',
-//     views: {
-//       navMenuView: { templateUrl: '/templates/developerViews/navMenu.html' },
-//       firstView: { templateUrl: 'templates/authentication/signoutView.html', controller: 'authController' }
-//     }
-//   })
-
-//   .state('sendReset', {
-//     url: '/{userType:donors|workers|admin}/send_reset',
-//     views: {
-//       navMenuView: { templateUrl: '/templates/developerViews/navMenu.html' },
-//       firstView: { templateUrl: 'templates/authentication/sendResetView.html', controller: 'authController' }
-//     }
-//   })
-
-//   .state('resetPassword', {
-//     url: '/{userType:donors|workers|admin}/reset_password/:resetToken',
-//     views: {
-//       navMenuView: { templateUrl: '/templates/developerViews/navMenu.html' },
-//       firstView: { templateUrl: 'templates/authentication/resetView.html', controller: 'authController' }
-//     }
-//   })
-//   //End Authentication Handlers
-
-// }])
-
-
-
-
 
 /*
-
-temporary object
+// CURRENT MASTER OBJ (as of 5/30 @ 13:00)
 {
   child: {
+    createdAt: 'dateString',
+    workerAccountID: 'someString',  // contains worker's account information (IE: supervisor name, coordinator name, department, etc.)
     firstName: 'Amar',
     lastName: 'Patel',
     phone: '19166008929',
@@ -318,76 +238,41 @@ temporary object
     location: 'San Francisco',
     CFID: '00001A',
     programArea: 'somewhere',
-    bio: 'Amar hails from a heritage of awesome and bad-assery.'
-  },
-
-  request: {
-    createdAt: 'dateString',
+    bio: 'Amar hails from a heritage of awesome and bad-assery.',
     items: [
       {
         item: 'bike', 
+        price: 'number',
         status: null, 
-        price: null, 
-        paymentDate: 'somedate',
         hsaReceivedDate: 'somedate',
         childReceivedDate: 'somedate'
       },
       {
         item: 'helmet', 
+        price: 'number',
         status: null, 
-        price: null, 
-        paymentDate: 'somedate',
         hsaReceivedDate: 'somedate',
         childReceivedDate: 'somedate'
       },
       {
         item: 'babes', 
+        price: 'number',
         status: null, 
-        price: null, 
-        paymentDate: 'somedate',
         hsaReceivedDate: 'somedate',
         childReceivedDate: 'somedate'
       }
-    ]
-  },
-
-  worker: {
-    accountID: 'string',
-    firstName: 'Wayland',
-    lastName: 'Gangsta-fresh',
-    phone: '12903812124',
-    email: 'someemail',
-    department: 'someDept',
-    supervisorFirstName: 'Marcus',
-    supervisorLastName: 'Phillips',
-    coordinatorFirstName: 'Tony',
-    coordinatorLastName 'Phillips'
-  },
-
-  donor: {
-    firstName: 'amar',
-    lastName: 'patel',
-    phone: '9166008929',
-    email: 'someemail',
-    address1: '1221 1st ave.',
-    address2: 'apt se1801',
-    city: 'seattle',
-    state: 'washington',
-    zip: '98101'
+    ],
+    donor: {
+      firstName: 'amar',
+      lastName: 'patel',
+      phone: '9166008929',
+      email: 'someemail',
+      address1: '1221 1st ave.',
+      address2: 'apt se1801',
+      city: 'seattle',
+      state: 'washington',
+      zip: '98101'
+    }
   }
 };
-
-*/
-
-
-// This module when invoked will see if the user is signed in and authorized to view the page. It returns a promise
-/* Valid parameters for protect = 'workers', 'donors', 'admin'
-  protect('donors').success(function(status){
-    if(status === false){
-      //User is not authorized, redirect to signin page
-    } else {
-      //User is authorized, serve normal page
-    }
-  });
-
 */
