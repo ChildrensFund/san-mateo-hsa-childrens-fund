cfa482253364b4820eadc0f8b668e56a888239cf
Evan Kim <evan.kim.390@gmail.com>
2014-05-30 14:01:37 -0700

    Set up scaffold for internal api (router + controller file)

---
+++ server/controllers/api_controller.js
@@ -0,0 +1,12 @@
+/*jslint node: true */
+'use strict';
+
+/*  This file contains the logic for handling requests submitted with the /donor
+ *  path.  */
+//    This file is required by (root folder)/server/routes/donor_routes.js
+
+var Donor = require('../config/mysql_config.js').Donor;
+var Staff = require('../config/mysql_config.js').Staff;
+var Admin = require('../config/mysql_config.js').Admin;
+var HelpDesk = require('../config/mysql_config.js').HelpDesk;
+
---
+++ server/routes/api_routes.js
@@ -0,0 +1,12 @@
+/*jslint node: true */
+'use strict';
+
+/*  This file contains the configuration for the ApiRouter (see
+ *  (root folder)/server/config/express_app.js). Requests with the '/api'
+ *  path are handled here. */
+//    This file is required by (root folder)/server/config/express_app.js
+var controller  = require( '../controllers/api_controller.js' );
+
+module.exports = function( router ) {
+
+};
