cb8bffff15a30e54f3dd55cd213a7e2cf7d2e6c9
Evan Kim <evan.kim.390@gmail.com>
2014-06-08 17:12:03 -0700

    Break factories and services out + update index.html to reflect changes

---
+++ client/js/factories/restful.js
@@ -0,0 +1,109 @@
+app.factory('restful', ['$http', '$cookies', function ($http, $cookies) {
+  return {
+
+    createChild: function (childObj) {
+      return $http({
+        method: 'POST',
+        url: '/api/workers/' + $cookies.id + '/children',
+        data: childObj
+      }).success(function (data, status) {
+        console.log('(postChild) POST Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(postChild) POST Error! ', data, status);
+      });
+    },
+
+    updateChild: function (childObj) {
+      return $http({
+        method: 'POST',
+        url: '/api/children/' + childObj.id,
+        data: childObj
+      }).success(function (data, status) {
+        console.log('(updateChild) POST Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(updateChild) POST Error! ', data, status);
+      });
+    },
+
+    getChildren: function (pageNumber) {
+      var queryUrl = '/api/children';
+      if(pageNumber){
+        queryUrl += '?page=' + pageNumber;
+      }
+      return $http({
+        method: 'GET',
+        url: queryUrl
+      }).success(function (data, status) {
+        console.log('(getChildren) GET Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(getChildren) GET Error! ', data, status);
+      });
+    },
+
+    getWorkersChildren: function () {
+      return $http({
+        method: 'GET',
+        url: '/api/workers/' + $cookies.id + '/children',
+      }).success(function (data, status) {
+        console.log('(getWorkersChildren) GET Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(getWorkersChildren) GET Error! ', data, status);
+      });
+    },
+
+    getWorkerData: function () {
+      return $http({
+        method: 'GET',
+        url: '/api/workers/' + $cookies.id
+      }).success(function (data, status) {
+        console.log('(getWorkerData) GET Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(getWorkerData) GET Error! ', data, status);
+      });
+    },
+
+    postWorkerData: function (workerObj) {
+      return $http({
+        method: 'POST',
+        url: '/api/workers/' + $cookies.id,
+        data: workerObj
+      }).success(function (data, status) {
+        console.log('(postWorkerData) POST Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(postWorkerData) POST Error! ', data, status);
+      });
+    },
+
+    getChildsDonor: function (id) {
+      return $http({
+        method: 'GET',
+        url: '/api/children/' + id + '/donor'
+      }).success(function (data, status) {
+        console.log('(getChildsDonor) GET Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(getChildsDonor) GET Error! ', data, status);
+      });
+    },
+
+    postDonor: function (postObj) {
+      return $http({
+        method: 'POST',
+        url: '/api/children/' + postObj.id + '/donor',
+        data: postObj.donor
+      }).success(function (data, status) {
+        console.log('(postDonor) POST Success! ', data);
+        return data;
+      }).error(function (data, status) {
+        console.log('(postDonor) POST Error! ', data, status);
+      });
+    }
+
+  }
+}]);
---
+++ client/js/services/sanitize.js
@@ -0,0 +1,44 @@
+app.service('sanitize', [function () {
+  return {
+    update: function (key, val) {
+      if (key === 'phone') {
+        var clean, sol = '';
+        if (val) {
+          clean = val.match(/\d+/g).join('').split('').reverse();
+          if (clean.length <= 11) {
+            for (var i=0;i<clean.length;i++) {
+              if (i === 4 || i === 7 || i === 10) {
+                sol += '-';
+              }
+              sol += clean[i];
+            }
+            return sol.split('').reverse().join('');
+          } else {
+            return clean.reverse().join('');
+          }
+        }
+        return undefined;
+      } else if (key.substr(key.length-5,key.length) === 'Price') {
+        if (val) {
+          if (val[0] === '$') {
+            val = val.slice(1);
+          }
+          return Number(val);
+        }
+        return undefined;
+      } else if (key === 'dob' || key.substr(key.length-4,key.length) === 'Date') {
+        return val.substr(0,4) + val.substr(4,6);
+      } else {
+        return val;
+      }
+    },
+
+    post: function () {},
+
+    get: function (key, val) {
+      if (key === 'dob' || key.substr(key.length-4,key.length) === 'Date') {
+        return val.substr(5,6) + '-' + val.substr(0,4);
+      }
+    }
+  }  
+}]);
